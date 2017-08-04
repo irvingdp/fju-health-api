84bb81f39227dc2ff513d3f8a5b8fd3272ed1e46
irvingdp <irvingdp@bitbucket.org>
2017-08-04 11:55:11 +0800

    add default data to package data.

--- build-resources/template/master/gen/knex/migrations/20170714000000_migration.js
+++ build-resources/template/master/gen/knex/migrations/20170714000000_migration.js
@@ -1,10 +1,12 @@
 /*
 <%= constant.warning %>
 */
-
+const GENDER = {
+    MALE : "male",
+    FEMALE : "female",
+}
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('User', function(t) {
-        t.charset("utf8");
         t.increments('id').unsigned().primary();
         t.string('email').notNull();
         t.string('uid').notNull();
@@ -13,7 +15,6 @@ exports.up = function (knex, Promise) {
         t.dateTime('updatedAt');
     }).then(function () {
         return knex.schema.createTable('Profile', function (t) {
-            t.charset("utf8");
             t.increments('id').unsigned().primary();
             t.string('name');
             t.dateTime('birthday');
@@ -26,18 +27,84 @@ exports.up = function (knex, Promise) {
         });
     }).then(function () {
         return knex.schema.createTable('Package', function (t) {
-            t.charset("utf8");
             t.increments('id').unsigned().primary();
+            t.integer('group');
+            t.integer('order');
             t.string('title');
             t.string('description');
-            t.integer('price');
+            t.string('price');
+            t.string('displayPrice');
             t.enu('gender', ['male', 'female']);
             t.dateTime('createdAt');
             t.dateTime('updatedAt');
         });
     }).then(function () {
+        let now = new Date().toISOString();
+        return knex('Package').insert([
+            {
+                group: 1,
+                order: 1,
+                title: "菁英防癌健檢",
+                description: "",
+                displayPrice: "NT$100,000",
+                price: 100000,
+                gender: GENDER.MALE,
+                createdAt: now,
+            },
+            {
+                group: 1,
+                order: 2,
+                title: "菁英防癌健檢",
+                description: "",
+                displayPrice: "NT$116,000",
+                price: 116000,
+                gender: GENDER.FEMALE,
+                createdAt: now,
+            },
+            {
+                group: 2,
+                order: 1,
+                title: "防癌健檢",
+                description: "",
+                displayPrice: "NT$71,000",
+                price: 71000,
+                gender: GENDER.MALE,
+                createdAt: now,
+            },
+            {
+                group: 2,
+                order: 2,
+                title: "防癌健檢",
+                description: "",
+                displayPrice: "NT$73,000",
+                price: 73000,
+                gender: GENDER.FEMALE,
+                createdAt: now,
+            },
+            {
+                group: 3,
+                order: 1,
+                title: "精準健檢",
+                description: "",
+                displayPrice: "NT$24,000",
+                price: 24000,
+                gender: GENDER.MALE,
+                createdAt: now,
+            },
+            {
+                group: 3,
+                order: 2,
+                title: "精準健檢",
+                description: "",
+                displayPrice: "NT$27,000",
+                price: 27000,
+                gender: GENDER.FEMALE,
+                createdAt: now,
+            },
+
+        ]);
+    }).then(function () {
         return knex.schema.createTable('Reservation', function (t) {
-            t.charset("utf8");
             t.increments('id').unsigned().primary();
             t.dateTime('reserveDate');
             t.enu('status', ['paymentPending', 'paymentCompleted']);
