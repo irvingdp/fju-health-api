18e6b7f5b0026ab7c99e183f9c4e474a64499bed
irvingdp <irvingdp@bitbucket.org>
2017-08-15 16:56:12 +0800

    add fetch package to /Dashboard

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -2,20 +2,25 @@ const express = require('express');
 const DomainUser = require('../domain/user');
 const DomainProfile = require('../domain/profile');
 const DomainReservation = require('../domain/reservation');
+const DomainPackage = require('../domain/package');
 
 let router = express.Router();
 let domainUser = new DomainUser();
 let domainProfile = new DomainProfile();
 let domainReservation = new DomainReservation();
+let domainPackage = new DomainPackage();
+
 
 router.get('/', async (req, res, next) => {
     try {
         let currentUser = await domainUser.getUser({email: req.authentication.email});
         let reservation = await domainReservation.getMyLatestReservationByUserId(currentUser.id);
         let profile = await domainProfile.getProfile(currentUser);
+        let package = await domainPackage.listPackages();
         res.status(200).json({
             reservation: reservation,
             profile: profile,
+            package: package,
         });
     } catch (error) {
         next(error);
