5cac0fb43293445d2e0c94e864a0a89a34d13831
irvingdp <irvingdp@bitbucket.org>
2017-08-29 11:37:40 +0800

    fix bug

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -11,15 +11,24 @@ let domainProfile = new DomainProfile();
 let domainReservation = new DomainReservation();
 let domainPackage = new DomainPackage();
 
-
+let sortByNotifyDate = (a, b) => {
+    if (a.notifyDate < b.notifyDate)
+        return -1;
+    if (a.notifyDate > b.notifyDate)
+        return 1;
+    return 0;
+}
 router.get('/', async (req, res, next) => {
     try {
         let currentUser = await domainUser.getUser({email: req.authentication.email});
         let reserved = await domainReservation.getMyLatestReservationByUserId(currentUser.id);
 
-        (reserved.reminder || []).forEach(reminder => {
-            reminder.notifyDate = reminderManager.getNotifyDate(reminder.key, reserved.reserveDate);
-        });
+        if(reserved && reserved.reminder instanceof Array) {
+            (reserved.reminder || []).forEach(reminder => {
+                reminder.notifyDate = reminderManager.getNotifyDate(reminder.key, reserved.reserveDate);
+            });
+            reserved.reminder && reserved.reminder.sort(sortByNotifyDate);
+        }
 
         let profile = await domainProfile.getProfile(currentUser);
         let package = await domainPackage.listPackages();
